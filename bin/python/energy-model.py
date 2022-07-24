f2ad7023492280aac0b153c0b7d9133e6192dc07
Mahsa Arabi <marabi@umass.edu>
2022-07-24 11:15:22 -0400

    Change list to array in electric energy model function

--- bin/python/energy-model.py
+++ bin/python/energy-model.py
@@ -90,13 +90,17 @@ def power_e(df_input):
     a = df.Acceleration
     m = df.Vehicle_mass+(df.Onboard*179)
     P_t = (eta_batt/eta_m*eta_d_beb)*(1/float(3600*eta_d_beb))*((1./25.92)*rho*C_D*A_f*v*v + m*g*C_r*(c1*v + c2)/1000 + 1.1*m*a)*v
+    #if a <0:
+    #    factor=np.exp(-(0.0411/abs(a)))
+    #else:
+    #    factor=1
     factor=[]
     for index, value in a.items():
         if value<0:
             factor.append(np.exp(-(0.0411/abs(a))))
         else:
             factor.append(1)
-    P_t_e=factor*P_t
+    P_t_e=np.array(factor)*P_t
     return P_t_e
 
 # Define fuel rate function for diesel vehicle
