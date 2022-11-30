16a2ec13ad8378a4b717fa0503e8074b63d6eaef
Mahsa Arabi <marabi@umass.edu>
2022-11-30 10:21:50 -0500

    Remap vehicle models

--- bin/python/map-vehicles.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jul 19 15:21:08 2022
-
-@author: Mahsa
-"""
-
---
+++ bin/python/optimize-unknown-models.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 29 21:03:49 2022
+
+@author: Mahsa
+"""
+
