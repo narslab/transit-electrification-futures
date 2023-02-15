b9182ec226b6284f06a16996a970f7799f614353
maharabi <87103328+maharabi@users.noreply.github.com>
2023-02-15 14:32:52 -0500

    Remove 19 validation points with refueling intervals less than an hour-change bus 7902 to XDE60 hybrid-new calibration

--- bin/python/calibration-irregular-intervals.py
+++ bin/python/calibration-irregular-intervals.py
@@ -172,8 +172,8 @@ def calibrate_parameter(start1, stop1, start2, stop2, hybrid=False):
         #parameter1=a0_heb
         #parameter2=a1_heb
         count=0
-        for a0 in np.linspace(start1, stop1, 100):
-            for a1 in np.linspace(start2, stop2, 100):
+        for a0 in np.linspace(start1, stop1, 20):
+            for a1 in np.linspace(start2, stop2, 20):
                 count+=1
                 print(count)
                 global a0_heb
@@ -291,8 +291,8 @@ def calibrate_parameter(start1, stop1, start2, stop2, hybrid=False):
 
 
 #conventional
-#calibrate_parameter(0.0001, 0.01, 0.000001, 0.0001, hybrid=False)
-calibrate_parameter(0.0005, 0.0030, 0.00001, 0.0002, hybrid=False)
+calibrate_parameter(0.0001, 0.01, 0.000001, 0.0001, hybrid=False)
+#calibrate_parameter(0.0005, 0.0030, 0.00001, 0.0002, hybrid=False)
 ##test
 #calibrate_parameter(0.0001, 0.0001, 0.000001, 0.000001, hybrid=False)
 
