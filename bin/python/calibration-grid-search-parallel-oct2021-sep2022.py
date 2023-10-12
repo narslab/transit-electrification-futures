b65539b14665a8eb3ab1b38ed1a52eab3f2b1d9c
maharabi <87103328+maharabi@users.noreply.github.com>
2023-10-12 17:29:47 -0400

    Add progress bar to calibration code

--- bin/python/calibration-grid-search-parallel-oct2021-sep2022.py
+++ bin/python/calibration-grid-search-parallel-oct2021-sep2022.py
@@ -157,7 +157,7 @@ def process_dataframe(df, validation, a0, a1, hybrid):
 
     df_integrated = df_integrated.merge(filtered_data, on=['Vehicle', 'ServiceDateTime'])
 
-    #print("df_integrated columns",df_integrated.columns)
+    print("df_integrated columns",df_integrated.columns)
     # Drop rows with NaN values in 'Energy' or 'Qty' columns
     df_integrated.dropna(subset=['Energy_sum', 'Qty'], inplace=True)
     df_integrated['Fuel_economy'] = np.divide(df_integrated['dist_sum'], df_integrated['Energy_sum'], where=df_integrated['Energy_sum'] != 0)
