86520effa8194cf5e15560b8dadce84578d31d6a
maharabi <87103328+maharabi@users.noreply.github.com>
2024-01-03 15:06:36 -0500

    calibration using 64 cores for calibrating gamma, eta_m and eta_driveline_beb simoltaniously

--- bin/python/calibration-grid-search-BEB-new-equation-01012024.py
+++ bin/python/calibration-grid-search-BEB-new-equation-01012024.py
@@ -151,12 +151,12 @@ def calibrate_parameter(args):
 
 
       results = pd.DataFrame(list(zip(parameter1_values,parameter2_values, RMSE_Energy_train, MAPE_Energy_train)),
-                             columns=['parameter1_values','parameter2_values', 'RMSE_Energy_train', 'MAPE_Energy_train'])
-      results.to_csv((r'../../results/calibration-grid-search-BEB-oct2021-sep2022_01022024.csv'))
+                             columns=['parameter1_values','parameter2_values', 'parameter3_values', 'RMSE_Energy_train', 'MAPE_Energy_train'])
+      results.to_csv((r'../../results/calibration-grid-search-BEB-oct2021-sep2022_01042024.csv'))
       print("--- %s seconds ---" % (time.time() - start_time))
 
     
-calibrate_parameter((0.001,5, 500, 0.9,0.99, 5, 0.9,0.99, 5))
+calibrate_parameter((0.01,10, 500, 0.9,0.99, 5, 0.9,0.99, 5))
 
 # Parallel version
 # def mean_absolute_percentage_error(y_true, y_pred):
